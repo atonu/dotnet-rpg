41f8c408f95a5b5cc7e2518b122776cfc5f5e4c5
ahmed zahin <atonu.zahin@gmail.com>
2021-05-23 02:35:29 +0600

    get methods created.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Name = "Sam", Class = RpgClass.Cleric}
+        };
+        [HttpGet("GetAll")] 
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        public ActionResult<Character> GetSingle()
+        {
+            return Ok(characters[0]);
+        }
+    }
+}
