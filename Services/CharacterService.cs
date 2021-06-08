ebc66a62789f1f3e5976915c648950a7f9264333
ahmed zahin <atonu.zahin@gmail.com>
2021-06-08 13:32:44 +0600

    Service responses created

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Services
@@ -11,20 +12,26 @@ namespace dotnet_rpg.Services
             new Character(),
             new Character { Id=1, Name = "Sam", Class = RpgClass.Cleric}
         };
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
+            var serviceResponse = new ServiceResponse<List<Character>>();
             characters.Add(newCharacter);
-            return (characters);
+            serviceResponse.Data = characters;
+            return serviceResponse;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-             return (characters);
+            var serviceRespose = new ServiceResponse<List<Character>>();
+            serviceRespose.Data = characters;
+            return serviceRespose;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-           return (characters.FirstOrDefault(c => c.Id == id));
+            var serviceResponse = new ServiceResponse<Character>();
+            serviceResponse.Data = characters.FirstOrDefault(c => c.Id == id);
+            return serviceResponse;
         }
     }
 }
